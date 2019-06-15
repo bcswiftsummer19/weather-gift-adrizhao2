ee6ce08b7e1f88289601287b6ededcbdce003114
Adrianna Zhao <zhaocx@bc.edu>
2019-06-15 17:24:22 -0400

    7.2 Complete

---
+++ WeatherGift/PageVC.swift
@@ -0,0 +1,20 @@
+//
+//  PageVC.swift
+//  WeatherGift
+//
+//  Created by Adrianna Zhao on 6/15/19.
+//  Copyright © 2019 Adrianna Zhao. All rights reserved.
+//
+
+import UIKit
+
+class PageVC: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
