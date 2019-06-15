42637f038ce922221a39c1c133534d0dde4487ea
Adrianna Zhao <zhaocx@bc.edu>
2019-06-15 17:11:00 -0400

    Beginning

---
+++ WeatherGift/DetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  DetailVC.swift
+//  WeatherGift
+//
+//  Created by Adrianna Zhao on 6/15/19.
+//  Copyright © 2019 Adrianna Zhao. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+ 
+
+}
---
+++ WeatherGift/ListVC.swift
@@ -0,0 +1,20 @@
+//
+//  ListVC.swift
+//  WeatherGift
+//
+//  Created by Adrianna Zhao on 6/15/19.
+//  Copyright © 2019 Adrianna Zhao. All rights reserved.
+//
+
+import UIKit
+
+class ListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
